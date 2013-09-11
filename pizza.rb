6b39b1fcd5803859ea84d5f2c0730c4237606539
Kaoru <dev@kaoruk.com>
2013-09-11 10:46:41 -0500

    Another failing test please

--- pizza.rb
+++ pizza.rb
@@ -4,11 +4,8 @@ module Pizza
   end
 
   class Topping
-    attr_accessor :name
-
     def initialize(name)
       @name = name
     end
-    
   end
 end
--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -4,15 +4,21 @@ require 'rspec'
 require_relative '../pizza'
 
 describe Pizza::Pie do
-  
+
 end 
 
 describe Pizza::Topping do
   describe '.initialize' do
     it "sets the name of the topping" do
-      topping = Pizza::Topping.new('olives')
+      topping = Pizza::Topping.new 'olives'
 
       expect(topping.name).to eq('olives')
+    end
+
+    it 'sets whether or not the topping is vegetarian' do
+      topping = Pizza::Topping.new 'bell peppers', vergetarian: true
+
+      expect(topping.vegetarian).to eq(true)
     end
   end
 end
